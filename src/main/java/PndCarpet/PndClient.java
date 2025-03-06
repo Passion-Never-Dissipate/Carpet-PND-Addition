e6f51f3dbf3ccadf22d90e998f6d52a27ac345e5
Bexerlmao <xu.luhuotangzhou@qq.com>
2025-03-07 04:01:49 +0800

    创建

---
+++ src/main/java/PndCarpet/PndClient.java
@@ -0,0 +1,19 @@
+package PndCarpet;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+import carpet.api.settings.CarpetRule;
+import carpet.api.settings.Rule;
+import carpet.api.settings.Validators;
+
+@Environment(EnvType.CLIENT)
+public class PndClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
---
+++ src/main/java/PndCarpet/PndServer.java
@@ -0,0 +1,12 @@
+package PndCarpet;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PndServer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+        System.out.println("Hello Fabric world!");
+    }
+}
